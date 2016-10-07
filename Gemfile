5df41a261a3240d41c693aa10d1e16beb58bf22b
Lukas Beaton <lukas.beaton@gmail.com>
2016-10-06 23:50:49 -0400

    setting up Gem

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ source "https://rubygems.org"
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
-  gem "shoulda", ">= 0"
+  gem "rspec", "2.14.0"
+  gem "byebug"
+  gem 'coveralls', require: false
   gem "rdoc", "~> 3.12"
   gem "bundler", "~> 1.0"
   gem "jeweler", "~> 2.0.1"
---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require(:development)
+
+require 'coveralls'
+Coveralls.wear!
+
+require 'rain_forest'
+
+module SimpleCov::Configuration
+  def clean_filters
+    @filters = []
+  end
+end
+
+SimpleCov.configure do
+  clean_filters
+  load_adapter 'test_frameworks'
+end
+
+ENV["COVERAGE"] && SimpleCov.start do
+end
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+end
