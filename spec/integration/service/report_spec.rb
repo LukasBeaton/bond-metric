bc3d196b5658d7c4ea355e1a7a8037da55051ac3
Lukas Beaton <lukas.beaton@gmail.com>
2016-10-08 00:17:55 -0400

    add some integration test cases for Report

---
+++ spec/integration/service/report_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Service::Report do
+  describe '#generate_spread_to_benchmark' do
+    it 'accepts a CSV file outputs the results' do
+      file_path = File.expand_path File.dirname(__FILE__)+"../../fixtures/spread_to_benchmark_good.csv"
+      
+      results = Service::Report.generate_spread_to_benchmark(file_path)
+
+      expect(results).to eq(<<-CSV)
+bond,benchmark,spread_to_benchmark
+C1,G1,1.60%
+C2,G3,2.80%
+CSV
+    end
+
+    it 'raises an exception when there is invalid data in the CSV file'
+  end
+
+  describe '#generate_spread_to_curve' do
+    it 'accepts a CSV file outputs the results' do
+      
+    end
+
+    it 'raises an exception when there is invalid data in the CSV file'
+  end
+end
