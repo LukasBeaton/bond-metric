3d10a2077aeca0abcef03fd26d4af14d4112a45d
Lukas Beaton <lukas.beaton@gmail.com>
2016-10-07 23:10:25 -0400

    fixed bug and added some more tests cases

--- lib/service/benchmark.rb
+++ lib/service/benchmark.rb
@@ -4,9 +4,12 @@ module Service
 
     def calculate_spread_to_benchmark(corporate_bond, government_bond)
       raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a corporate bond as its first parameter!" if corporate_bond.type != BondMetric::CORPORATE
-      raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a government bond as its second parameter!"  if corporate_bond.type != BondMetric::GOVERNMENT
+      raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a government bond as its second parameter!"  if government_bond.type != BondMetric::GOVERNMENT
 
       return corporate_bond.basis_points - government_bond.basis_points
     end 
+
+    def calculate_spread_to_curve(corporate_bond, government_bond_less_term, government_bond_greater_term)
+    end
   end
 end
--- spec/unit/service/benchmark_spec.rb
+++ spec/unit/service/benchmark_spec.rb
@@ -24,7 +24,6 @@ describe Service::Benchmark do
     end
 
     context 'failure' do
-      
       it 'must have a corporate bond as the first parameter' do
         g2 = Struct::Bond.new('G2', 'government', 10.3, 555)
 
@@ -44,14 +43,27 @@ describe Service::Benchmark do
   end
 
   describe '#calculate_spread_to_curve' do
+    before do
+      @C1 = Struct::Bond.new('C1', 'corporate', 10.3, 530)
+      @G1 = Struct::Bond.new('G1', 'government', 9.4, 370)
+      @G2 = Struct::Bond.new('G2', 'government', 12, 480)
+    end
+    
     context 'success' do
-      it 'can calculate a positive spread'
+      it 'can calculate a positive spread' do
+        result = Service::Benchmark.calculate_spread_to_curve(@C1, @G1, @G2)
+
+        expect(result).to eq(122)
+      end
+      
       it 'can calulate a negative spread'
     end
 
     context 'failure' do
       it 'must have a corporate bond as the first parameter'
       it 'must have a government bond as the second parameter'
+      it 'must have a government bond as the third parameter'
+      it 'must have a corporate bond with a term in between the government bonds'
     end
   end
 end
