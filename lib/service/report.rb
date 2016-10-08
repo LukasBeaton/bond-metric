460ee1479da5d5d13fd5b2f6d49805252d2471ae
Lukas Beaton <lukas.beaton@gmail.com>
2016-10-08 02:16:25 -0400

    small refactor

--- lib/service/report.rb
+++ lib/service/report.rb
@@ -1,28 +1,5 @@
 require 'csv'
 
-Struct.new("SpreadToBenchmark", :corporate, :government) do
-  def proximity
-    Service::Benchmark.calculate_proximity(corporate, government)
-  end
-
-  def spread
-    Service::Benchmark.calculate_spread_to_benchmark(corporate, government)
-  end
-
-  def print_spread
-    decimal = (spread / 100.0)
-    sprintf("%.2f%", decimal)
-  end
-  
-  def report_header
-    ['bond', 'benchmark', 'spread_to_benchmark']
-  end
-
-  def report_record 
-    [corporate.bond, government.bond, print_spread]
-  end  
-end  
-
 module Service
   class Report
     def initialize(file_path)
@@ -82,3 +59,26 @@ module Service
     end
   end
 end
+
+Struct.new("SpreadToBenchmark", :corporate, :government) do
+  def proximity
+    Service::Benchmark.calculate_proximity(corporate, government)
+  end
+
+  def spread
+    Service::Benchmark.calculate_spread_to_benchmark(corporate, government)
+  end
+
+  def print_spread
+    decimal = (spread / 100.0)
+    sprintf("%.2f%", decimal)
+  end
+  
+  def report_header
+    ['bond', 'benchmark', 'spread_to_benchmark']
+  end
+
+  def report_record 
+    [corporate.bond, government.bond, print_spread]
+  end  
+end  
