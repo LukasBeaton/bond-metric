f267a417f8c08aff962e7f79b239ab873ee98b37
Lukas Beaton <lukas.beaton@gmail.com>
2016-10-07 23:47:49 -0400

    refactor

--- lib/service/benchmark.rb
+++ lib/service/benchmark.rb
@@ -3,18 +3,13 @@ module Service
     extend self
 
     def calculate_spread_to_benchmark(corporate_bond, government_bond)
-      raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a corporate bond as its first parameter!" if corporate_bond.type != BondMetric::CORPORATE
-      raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a government bond as its second parameter!"  if government_bond.type != BondMetric::GOVERNMENT
+      validate_spread_to_benchmark_params(corporate_bond, government_bond)
 
       return corporate_bond.basis_points - government_bond.basis_points
     end 
 
     def calculate_spread_to_curve(corporate_bond, government_bond_lesser, government_bond_greater)
-      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a corporate bond as its first parameter!" if corporate_bond.type != BondMetric::CORPORATE
-      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a government bond as its second parameter!" if government_bond_lesser.type != BondMetric::GOVERNMENT
-      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a government bond as its third parameter!" if government_bond_greater.type != BondMetric::GOVERNMENT
-      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a first government bond parameter with a term that is less than the second government bond parameter!" if government_bond_lesser.term_years > government_bond_greater.term_years
-      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a corporate bond with a term between both of the government bonds!" if corporate_bond.term_years < government_bond_lesser.term_years || corporate_bond.term_years > government_bond_greater.term_years
+      validate_spread_to_curve(corporate_bond, government_bond_lesser, government_bond_greater)
 
       x = corporate_bond.term_years
       x1 = government_bond_lesser.term_years
@@ -25,6 +20,21 @@ module Service
       y = (y1 + ((x - x1)*(y2 - y1))/(x2 - x1)).round
 
       return corporate_bond.basis_points - y
+    end
+
+    private
+
+    def validate_spread_to_benchmark_params(corporate_bond, government_bond)
+      raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a corporate bond as its first parameter!" if corporate_bond.type != BondMetric::CORPORATE
+      raise "ERROR: Service::Benchmark#calculate_spread_to_benchmark MUST have a government bond as its second parameter!"  if government_bond.type != BondMetric::GOVERNMENT
+    end
+
+    def validate_spread_to_curve(corporate_bond, government_bond_lesser, government_bond_greater)
+      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a corporate bond as its first parameter!" if corporate_bond.type != BondMetric::CORPORATE
+      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a government bond as its second parameter!" if government_bond_lesser.type != BondMetric::GOVERNMENT
+      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a government bond as its third parameter!" if government_bond_greater.type != BondMetric::GOVERNMENT
+      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a first government bond parameter with a term that is less than the second government bond parameter!" if government_bond_lesser.term_years > government_bond_greater.term_years
+      raise "ERROR: Service::Benchmark#calculate_spread_to_curve MUST have a corporate bond with a term between both of the government bonds!" if corporate_bond.term_years < government_bond_lesser.term_years || corporate_bond.term_years > government_bond_greater.term_years
     end
   end
 end
